feab1cad8c97d37cb2f9161a349227e8e0160f33
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-05-15 15:43:10 +0800

    did little redesign

--- layout/header.php
+++ layout/header.php
@@ -1,42 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<header>
+	<h1>EncodeHub</h1>
+	<nav>
+		<a href="/hacking_conversions_website/index.html">Home</a>
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>EncodeHub - Home</title>
-</head>
+		<!-- <h2>Encryption/Decryption</h2> -->
+		<a href="/hacking_conversions_website/public/caesar.php">Caesar Cipher</a>
+		<a href="/hacking_conversions_website/public/vigenere.php">Vigenère Cipher</a>
+		<a href="/hacking_conversions_website/public/aes.php">AES</a>
+		<a href="/hacking_conversions_website/public/rsa.php">RSA</a>
 
-<body>
-	<header>
-		<h1>EncodeHub</h1>
-		<nav>
-			<a href="/hacking_conversions_website/index.html">Home</a>
-			<!-- <h2>Encryption/Decryption</h2> -->
-			<a href="/hacking_conversions_website/public/caesar.php">Caesar Cipher</a>
-			<a href="/hacking_conversions_website/public/vigenere.php">Vigenère Cipher</a>
-			<a href="aes.php">AES</a>
-			<a href="rsa.php">RSA</a>
+		<!-- <h2>Encoding/Decoding</h2> -->
+		<a href="/hacking_conversions_website/public/base32.php">Base32</a>
+		<a href="/hacking_conversions_website/public/base64.php">Base64</a>
+		<a href="/hacking_conversions_website/public/hex.php">Hexadecimal</a>
 
-			<!-- <h2>Encoding/Decoding</h2> -->
-			<a href="base64.php">Base64</a>
-			<a href="base32.php">Base32</a>
-			<a href="hex.php">Hexadecimal</a>
+		<!-- <h2>Hashing</h2> -->
+		<a href="/hacking_conversions_website/public/md5.php">MD5</a>
+		<a href="/hacking_conversions_website/public/sha1.php">SHA-1</a>
+		<a href="/hacking_conversions_website/public/sha256.php">SHA-256</a>
 
-			<!-- <h2>Hashing</h2> -->
-			<a href="sha256.php">SHA-256</a>
-			<a href="md5.php">MD5</a>
-			<a href="sha1.php">SHA-1</a>
+		<!-- <h2>Numerical Conversions</h2> -->
+		<a href="/hacking_conversions_website/public/binary.php">Binary/Decimal/Hexadecimal</a>
 
-			<!-- <h2>Numerical Conversions</h2> -->
-			<a href="binary.php">Binary/Decimal/Hexadecimal</a>
-
-			<!-- <h2>Visualization</h2> -->
-			<a href="visualize.php">Visualize</a>
-
-			<!-- more links -->
-		</nav>
-	</header>
-</body>
-
-</html>
+		<!-- <h2>Visualization</h2> -->
+		<a href="/hacking_conversions_website/public/visualize.php">Visualize</a>
+	</nav>
+</header>
